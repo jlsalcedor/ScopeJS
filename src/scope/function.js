e33d814393f0301dea091e98de9d1c6ebc8baea8
john leandro <jlsrapp1990@gmail.com>
2023-08-04 15:31:26 -0500

    Scopes local and global

---
+++ src/scope/function.js
@@ -0,0 +1,15 @@
+
+function greeting(){
+
+    let person = 'Ana';
+
+    console.log(person);
+
+    if (person === 'Ana'){
+        console.log(`Hello ${person}!`);
+    }
+}
+
+greeting();
+
+console.log(person);
